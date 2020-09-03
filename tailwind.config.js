e6a61aa64a740e469882867efd5c65b33ae44dc4
dboudro <dylanrboudro@gmail.com>
2020-09-02 18:09:38 -0700

    major redesign

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: {
+          '100': '#fff7e5',
+          '200': '#ffe4b3',
+          '300': '#f8cc87',
+          '400': '#e3af68',
+          '500': '#d39245',
+          '600': '#ba792c',
+          '700': '#975a1c',
+          '800': '#783d07',
+          '900': '#4d2200',
+        }
+      }
+    }
   },
   variants: {},
   plugins: [],
